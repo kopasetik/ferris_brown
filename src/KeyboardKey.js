d82196b1dd5cfe1de8993f83007652f6c4a131d7
Wale O <dev@screenverve.com>
2020-03-28 04:46:08 +0000

    add note letters to keyboard display

--- src/KeyboardKey.js
+++ src/KeyboardKey.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './KeyboardKey.css';
 
-function KeyboardKey({format}) {
+function KeyboardKey({format, children}) {
   return (
     <div className={(format === "white")? "WhiteKey": "BlackKey"}>
+	  {children}
     </div>
   );
 }
