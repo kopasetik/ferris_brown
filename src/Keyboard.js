a6763310fd05790ec6654e2af013a9aa7058dc63
Wale O <dev@screenverve.com>
2020-04-01 16:59:15 +0000

    cut keyboard down to an octave

--- src/Keyboard.js
+++ src/Keyboard.js
@@ -24,18 +24,6 @@ class Keyboard extends React.Component {
 				{note: 'Bb', color: 'black'},
 				{note: 'B', color: 'white'},
 				{note: 'C', color: 'white'},
-	//			{note: 'C#', color: 'black'},
-	//			{note: 'D', color: 'white'},
-	//			{note: 'Eb', color: 'black'},
-	//			{note: 'E', color: 'white'},
-	//			{note: 'F', color: 'white'},
-	//			{note: 'F#', color: 'black'},
-	//			{note: 'G', color: 'white'},
-	//			{note: 'Ab', color: 'black'},
-	//			{note: 'A', color: 'white'},
-	//			{note: 'Bb', color: 'black'},
-	//			{note: 'B', color: 'white'},
-	//			{note: 'C', color: 'white'},
 			]
 		}
 
