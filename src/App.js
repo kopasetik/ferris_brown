aa479ae877186e7cc718a716f6db935eb47ac4c0
Wale O <dev@screenverve.com>
2020-04-12 03:29:24 +0000

    remove dropdowns from App component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
 import Keyboard from './Keyboard.js';
-import Dropdowns from './Dropdowns.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-	  <Dropdowns />
 	  <Keyboard />
       </header>
     </div>
