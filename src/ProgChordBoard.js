55e5e995168de255d16c2fa3d1ddaff8c3d249ff
Wale O <dev@screenverve.com>
2020-06-16 19:18:57 +0000

    make sequences that loop and change chords to "Strawberry Number 23"

--- src/ProgChordBoard.js
+++ src/ProgChordBoard.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './ProgChordBoard.css';
 import Voicing from './Voicing'
 import ProgPlayButton from './ProgPlayButton'
-import { Transport, PolySynth } from 'tone';
+import { Sequence, PolySynth, Transport } from 'tone';
 import Teoria from 'teoria'
 
 const synth = new PolySynth(6).toMaster()
@@ -14,16 +14,17 @@ class ProgChordBoard extends React.Component {
 			currentScale: 'Bb',
 			currentMode: 'major',
 			chords: [
-				{chord: 'Bm'},
-				{chord: 'Am7'},
-				{chord: 'D9'},
-				{chord: 'G7'},
+				{chord: 'Am'},
+				{chord: 'Dm'},
+				{chord: 'Em'},
+				{chord: 'F'},
+				{chord: 'G'},
 			],
 		}
 		
 		this.attackChord = this.attackChord.bind(this)
 		this.releaseChord = this.releaseChord.bind(this)
-		this.playTransport = this.playTransport.bind(this)
+		this.playSequence = this.playSequence.bind(this)
 		this.giveChordsNotes = this.giveChordsNotes.bind(this)
 	}
 
@@ -35,15 +36,20 @@ class ProgChordBoard extends React.Component {
 		synth.triggerRelease(chordVoicing)
 	}
 
-	playTransport(chordNotes){
-		Transport.scheduleRepeat(function(time){
-			chordNotes.map((notes, idx) => {
-					const timing = (idx > 0) ? `${idx}m` : undefined
-					synth.triggerAttackRelease(notes, '16n', timing)
-				})
-		}, "4n")
+	playSequence(chordNotes){
+		const chordIndexes = chordNotes.map((chord, idx) => idx)		
 
+		const seq = new Sequence(function(val, i){
+			synth.triggerAttackRelease(chordNotes[i], '8n')
+		}, chordIndexes, '4n')
+
+		seq.start(0)
+		seq.loop = 3
+		seq.humanize = true
+
+		Transport.bpm.value = 67
 		Transport.start()
+
 	}
 
 	giveChordsNotes(chords){
@@ -55,7 +61,6 @@ class ProgChordBoard extends React.Component {
 
 		const chordLengthsInSequence = chords
 			.map(newChord => Teoria.chord(newChord).simple())
-    			//.map(newChord => newChord.simple())
     			.map(chord => chord.length)
     			.reduce((acc, curr, idx) => [...acc,(acc[acc.length-1] || 0) + curr],[])
 
@@ -90,7 +95,7 @@ class ProgChordBoard extends React.Component {
 				<ProgPlayButton 
 					chords={this.state.chords.map(({chord}) => chord)}
 					chordNotes={destructuredOctavedChords} 
-					playAction={this.playTransport} 
+					playAction={this.playSequence} 
 				/>
 			</div>)
 		}
